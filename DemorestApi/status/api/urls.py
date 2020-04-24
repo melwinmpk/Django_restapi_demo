2bd6588c4de48c294e07485bd04e6fc5759f5bd8
Melwinmpk <melwinmpk13@gmail.com>
2020-04-24 08:23:53 +0530

    Update & Delete API Views

--- DemorestApi/status/api/urls.py
+++ DemorestApi/status/api/urls.py
@@ -6,7 +6,9 @@ from .views import (
     StatusListSearchAPIView,
     StatusAPIView,
     StatusCreateAPIView,
-    StatusDetailAPIView
+    StatusDetailAPIView,
+    StatusUpdateAPIView,
+    StatusDeleteAPIView
 )
 urlpatterns =[
     path('', StatusAPIView.as_view()),
@@ -14,7 +16,7 @@ urlpatterns =[
     # path('', StatusListSearchAPIView.as_view()),
     # path('create/', StatusCreateAPIView.as_view()),
     path('<int:id>',StatusDetailAPIView.as_view()),
-    # path('<int:id>/update/',StatusUpdateAPIView.as_view()),
-    # path('<int:id>/delete/',StatusDeleteAPIView.as_view()),
+    path('<int:id>/update/',StatusUpdateAPIView.as_view()),
+    path('<int:id>/delete/',StatusDeleteAPIView.as_view()),
 ]
 #  api/status/create
--- DemorestApi/status/api/views.py
+++ DemorestApi/status/api/views.py
@@ -56,3 +56,17 @@ class StatusDetailAPIView(generics.RetrieveAPIView):
     #     kwargs = self.kwargs
     #     kw_id  = kwargs.get('id')
     #     return Status.objects.get(id=kw_id)
+
+class StatusUpdateAPIView(generics.UpdateAPIView):
+    permission_classes          = []
+    authentication_classes      = []
+    queryset                    = Status.objects.all()
+    serializer_class            = StatusSerializer
+    lookup_field = 'id'  # if the lookup_field is not defined then pk will be expected by default
+
+class StatusDeleteAPIView(generics.DestroyAPIView):
+    permission_classes          = []
+    authentication_classes      = []
+    queryset                    = Status.objects.all()
+    serializer_class            = StatusSerializer
+    lookup_field = 'id'  # if the lookup_field is not defined then pk will be expected by default

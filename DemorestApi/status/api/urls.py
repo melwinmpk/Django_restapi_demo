cc37468fbb0dbb8841939a895a1dedff8fe9c8f2
Melwinmpk <melwinmpk13@gmail.com>
2020-04-25 18:49:46 +0530

    One Api Endpoint for CRUDL

--- DemorestApi/status/api/urls.py
+++ DemorestApi/status/api/urls.py
@@ -6,7 +6,7 @@ from .views import (
     # StatusListSearchAPIView,
     StatusAPIView,
     # StatusCreateAPIView,
-    StatusDetailAPIView,
+    # StatusDetailAPIView,
     # StatusUpdateAPIView,
     # StatusDeleteAPIView
 )
@@ -15,7 +15,7 @@ urlpatterns =[
     # path('create',StatusCreateAPIView.as_view()),
     # path('', StatusListSearchAPIView.as_view()),
     # path('create/', StatusCreateAPIView.as_view()),
-    path('<int:id>',StatusDetailAPIView.as_view()),
+    # path('<int:id>',StatusDetailAPIView.as_view()),
     # path('<int:id>/update/',StatusUpdateAPIView.as_view()),
     # path('<int:id>/delete/',StatusDeleteAPIView.as_view()),
 ]
--- DemorestApi/status/api/views.py
+++ DemorestApi/status/api/views.py
@@ -3,37 +3,58 @@ from rest_framework.views import APIView
 from  rest_framework.response import Response
 from status.models import Status
 from .serializers import StatusSerializer
+from django.shortcuts import get_object_or_404
 # from django.views.generic
 
-class StatusListSearchAPIView(APIView):
-    permission_classes          = []
-    authentication_classes      = []
 
-    def get(self, request, format=None):
-        qs = Status.objects.all()
-        serializer = StatusSerializer(qs, many=True)
-        return Response(serializer.data)
 
-    def post(self, request, format=None):
-        qs = Status.objects.all()
-        serializer = StatusSerializer(qs, many=True)
-        return Response(serializer.data)
-
-class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+class StatusAPIView(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.ListAPIView):
     permission_classes          = []
     authentication_classes      = []
     # queryset                    = Status.objects.all()
     serializer_class            = StatusSerializer
 
     def get_queryset(self):
+        request = self.request
         qs = Status.objects.all()
-        query = self.request.GET.get('q')
+        query = request.GET.get('q')
         if query is not None:
             qs = qs.filter(content__icontains=query)
         return qs
 
+    def get_object(self):
+        request   = self.request
+        passed_id = request.GET.get('id', None)
+        queryset   = self.get_queryset()
+        obj        = None
+        if passed_id is not None:
+            obj = get_object_or_404(queryset, id=passed_id)
+            self.check_object_permissions(request, obj)
+        return obj
+
+    def get(self, request, *args, **kwargs):
+        passed_id = request.GET.get('id', None)
+        if passed_id is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
 
 '''
     StatusDetailAPIView which is commented is equivalent to the StatusDetailAPIView which is not
@@ -72,6 +93,19 @@ class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, gen
 '''
     Currently not in use the below code
 '''
+class StatusListSearchAPIView(APIView):
+    permission_classes          = []
+    authentication_classes      = []
+
+    def get(self, request, format=None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+        return Response(serializer.data)
 
 class StatusCreateAPIView(generics.CreateAPIView):
     permission_classes          = []

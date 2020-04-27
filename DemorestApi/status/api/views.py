5256ae6ab5ef19899c82f4a13a23eb01e12f439a
Melwinmpk <melwinmpk13@gmail.com>
2020-04-27 08:36:29 +0530

    Views for CRUDL

--- DemorestApi/status/api/views.py
+++ DemorestApi/status/api/views.py
@@ -15,6 +15,32 @@ def is_json(json_data):
     except ValueError:
         is_valid = False
     return is_valid
+
+class StatusDetailAPIView(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    generics.RetrieveAPIView):
+    permission_classes          = []
+    authentication_classes      = []
+    # queryset                    = Status.objects.all()
+    serializer_class            = StatusSerializer
+    lookup_field                = 'id' # if the lookup_field is not defined then pk will be expected by default
+
+    def get_object(self, *args, **kwargs): # slug method for handeling the
+        kwargs = self.kwargs
+        kw_id  = kwargs.get('id')
+        return Status.objects.get(id=kw_id)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
 class StatusAPIView(
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
@@ -35,108 +61,16 @@ class StatusAPIView(
             qs = qs.filter(content__icontains=query)
         return qs
 
-    def get_object(self):
-        request   = self.request
-        passed_id = request.GET.get('id', None) or self.passed_id
-        queryset   = self.get_queryset()
-        obj        = None
-        if passed_id is not None:
-            obj = get_object_or_404(queryset, id=passed_id)
-            self.check_object_permissions(request, obj)
-        return obj
-
-    '''For the Older framework version the perform_destroy method was required to Delete a instance'''
-    # def perform_destroy(self, instance):
-    #     if instance is not None:
-    #         return instance.delete()
-    #     return None
-
-    def get(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id', None)
-        json_data     = {}
-        body_         = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id', None)
-        print(request.body)
-        passed_id = url_passed_id or new_passed_id or None
-        self.passed_id = passed_id
-        if passed_id is not None:
-            return self.retrieve(request, *args, **kwargs)
-        return super().get(request, *args, **kwargs)
-
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id', None)
-        json_data = {}
-        body_ = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id', None)
-        print(request.body)
-        passed_id = url_passed_id or new_passed_id or None
-        self.passed_id = passed_id
-        return self.update(request, *args, **kwargs)
 
-    def patch(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id', None)
-        json_data = {}
-        body_ = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id', None)
-        print(request.body)
-        passed_id = url_passed_id or new_passed_id or None
-        self.passed_id = passed_id
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id', None)
-        json_data = {}
-        body_ = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id', None)
-        print(request.body)
-        passed_id = url_passed_id or new_passed_id or None
-        self.passed_id = passed_id
-        return self.destroy(request, *args, **kwargs)
 
 
 '''
     StatusDetailAPIView which is commented is equivalent to the StatusDetailAPIView which is not
     uncommented one used mixins individually but the generic api view uses combined 
     '''
-
-# class StatusDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-#     permission_classes = []
-#     authentication_classes = []
-#     queryset = Status.objects.all()
-#     serializer_class = StatusSerializer
-#     lookup_field = 'id'  # if the lookup_field is not defined then pk will be expected by default
-
-class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
-    permission_classes          = []
-    authentication_classes      = []
-    queryset                    = Status.objects.all()
-    serializer_class            = StatusSerializer
-    lookup_field                = 'id' # if the lookup_field is not defined then pk will be expected by default
-
-    # def get_object(self, *args, **kwargs): # slug method for handeling the
-    #     kwargs = self.kwargs
-    #     kw_id  = kwargs.get('id')
-    #     return Status.objects.get(id=kw_id)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 
 '''
